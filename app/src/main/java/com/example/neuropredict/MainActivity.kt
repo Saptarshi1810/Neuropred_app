d2856e78cc5116c6ede40389b479f8b10d837ff9
Suswan114 <infinityplaycons04@gmail.com>
2024-04-10 22:25:59 +0530

    Appointment page added

--- app/src/main/java/com/example/neuropredict/MainActivity.kt
+++ app/src/main/java/com/example/neuropredict/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.example.neuropredict
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,7 @@ class MainActivity : AppCompatActivity() {
         val tappingGameButton=findViewById<CardView>(R.id.tapping_game_btn)
         val identifyGameButton=findViewById<CardView>(R.id.identify_game_btn)
         val historyButton = findViewById<CardView>(R.id.history)
+        val bookAppointment =findViewById<CardView>(R.id.appointment)
         tappingGameButton.setOnClickListener{
             val intent = Intent(this,TappingGameActivity::class.java)
             startActivity(intent)
@@ -23,6 +25,11 @@ class MainActivity : AppCompatActivity() {
         historyButton.setOnClickListener {
             val intent = Intent(this,HistoryActivity::class.java)
             startActivity(intent)
+        }
+
+        bookAppointment.setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://neurokolkata.org/appointment/"))
+            startActivity(browserIntent)
         }
     }
 }
